3cdece278d7cd46514e4e972c0955c9da6898f75
tagawa shouta <sho.webfe@gmail.com>
2022-06-20 02:17:26 +0900

    graphqlのセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { User } from './user/user';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,16 +15,27 @@ import { UserModule } from './user/user.module';
       envFilePath: ['.env'],
       isGlobal: true
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      },
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      entities: [
+        User
+      ],
+      synchronize: true,
     }),
     UserModule
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
